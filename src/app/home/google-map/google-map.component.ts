274eee1fe2f876a4841026f3a2b028410fa3467a
JStuve <stuve.josh@gmail.com>
2018-04-03 14:17:10 -0500

    Successfully styles map

--- src/app/home/google-map/google-map.component.ts
+++ src/app/home/google-map/google-map.component.ts
@@ -8,9 +8,162 @@ import { Component, OnInit } from '@angular/core';
 
 export class GoogleMapComponent implements OnInit {
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-  
+  lat: number = 33.571901;
+  lng: number = -101.861329;
+
+  styles = [
+   {
+       "featureType": "landscape.natural",
+       "elementType": "geometry.fill",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "color": "#e0efef"
+           }
+       ]
+   },
+   {
+       "featureType": "poi",
+       "elementType": "geometry.fill",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "hue": "#1900ff"
+           },
+           {
+               "color": "#c0e8e8"
+           }
+       ]
+   },
+   {
+       "featureType": "road",
+       "elementType": "geometry",
+       "stylers": [
+           {
+               "lightness": 100
+           },
+           {
+               "visibility": "simplified"
+           }
+       ]
+   },
+   {
+       "featureType": "road",
+       "elementType": "labels",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "all",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "geometry",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "geometry.fill",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "geometry.stroke",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "labels",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "labels.text",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "labels.text.fill",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "labels.text.stroke",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "road.local",
+       "elementType": "labels.icon",
+       "stylers": [
+           {
+               "visibility": "on"
+           }
+       ]
+   },
+   {
+       "featureType": "transit.line",
+       "elementType": "geometry",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "lightness": 700
+           }
+       ]
+   },
+   {
+       "featureType": "water",
+       "elementType": "all",
+       "stylers": [
+           {
+               "color": "#7dcdcd"
+           }
+       ]
+   }
+]
+
   constructor() { }
 
   ngOnInit() {
