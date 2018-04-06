4a2e8991e36e4b7abf4726d3cb904dd37c35356b
JStuve <stuve.josh@gmail.com>
2018-04-06 10:52:07 -0500

    Preped function to update event on RSVP

--- src/app/home/view-event/view-event.component.ts
+++ src/app/home/view-event/view-event.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import { EventService } from './event/event.service';
+
 @Component({
   selector: 'app-view-event',
   templateUrl: './view-event.component.html',
-  styleUrls: ['./view-event.component.css']
+  styleUrls: ['./view-event.component.css'],
+  providers: []
 })
 export class ViewEventComponent implements OnInit {
 
   @Input() currentEvent: object = {};
 
-  constructor() {
-
-  }
+  constructor() { }
 
   ngOnInit() {
 
@@ -21,4 +22,19 @@ export class ViewEventComponent implements OnInit {
     var viewEventBtn: HTMLElement = document.getElementById("viewEventBtn") as HTMLElement;
     viewEventBtn.click();
   }
+
+  updateRSVP(){
+
+    console.log(this.currentEvent);
+    this.currentEvent.rsvpPeople++;
+  //
+  //   this.eventService.editEventWithId("5ac6e17510988a56e8cd8b2d", this.currentEvent).subscribe(
+  //     response => {
+  //       console.log(response);
+  //     },
+  //     error => {
+  //       this.profileSettings.triggerNewUserModal(true);
+  //     }
+  //   );
+  // }
 }
