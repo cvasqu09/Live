31abe3bc86284688f733b2b187ce7b9e67b15838
Chris.V <cvasqu09@gmail.com>
2018-04-09 21:14:57 -0500

    Reset the date input field after successfully creating an event

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, ViewChild, ElementRef} from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, ViewChild, ElementRef } from '@angular/core';
+import { NgbDateStruct, NgbInputDatepicker } from '@ng-bootstrap/ng-bootstrap';
 
 const now = new Date();
 
@@ -11,7 +11,7 @@ const now = new Date();
 export class DatePickerComponent implements OnInit, OnChanges {
 	// Used to emit the date that changed to the parent component
 	@Input() formReset: boolean;
-	@ViewChild('datepicker') datePicker: ElementRef;
+	@ViewChild('datepicker') datePicker;
 	@Output() dateEmitter: EventEmitter<{year: number, month: number, day: number}> = new EventEmitter<{year: number, month: number, day: number}>();
 	model: NgbDateStruct;
 
@@ -23,7 +23,7 @@ export class DatePickerComponent implements OnInit, OnChanges {
   // Called when @Input is changed. formReset is changed in the event component when submitting the form is successful
   ngOnChanges() {
   	if(this.formReset == true){
-  		// Reset the dates since this is not automatically handled with resetting the form
+  		// Reset the dates input field since this is not automatically handled with resetting the form
  			this.datePicker._elRef.nativeElement.value = "";
   	}
   }
